35bfff21b50b13733aeb46d07c44dfe436a8283d
Tomas Casey <tomasmcasey@gmail.com>
2022-11-23 18:19:31 -0300

    Ejercicio resuelto yelp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,36 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit,]
+  # GET /restaurants
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET /restaurants/1
+  def show
+  end
+
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # POST /restaurants
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
